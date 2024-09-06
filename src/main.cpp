d1417ce96a1bd102c41e42f5d2e3f6bc1e27ca86
Benjamin Krieger <bkrieger@igus.net>
2024-09-06 13:53:02 +0200

    Add random track playback, since reading filecount now works. Add explanation for PIR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,12 @@
 
 // === Settings ===
 // DFPlayer settings
-#define PLAYER_VOLUME 5
+#define PLAYER_VOLUME 30
 #define PLAYER_TIMEOUT 2000
 // Time between the end of one track and the start of another
 #define TRACK_DELAY 5000
 // Delay at the end of each loop
-#define LOOP_DELAY 800
+#define LOOP_DELAY 400
 // Delay after starting a track with next()
 #define PLAYER_PLAY_DELAY 500
 // Setup delay after initializing
@@ -34,40 +34,40 @@
 unsigned long lastTrackFinished = 0;
 bool isPlaying = false;
 bool debug = false;
+bool useRandom = false;
+int fileCount = -1;
 
 SoftwareSerial playerSerial(PLAYER_TX_PIN, PLAYER_RX_PIN);
 DFRobotDFPlayerMini dfPlayer; 
 
 void print(String m);
 void println(String m);
+void errorState();
 
 void setup() {
+  // Configure pins
   pinMode(LED_PIN, OUTPUT);
   pinMode(DEBUG_PIN, INPUT);
   pinMode(SENSOR_PIN, INPUT);
   pinMode(BUSY_PIN, INPUT_PULLUP);
 
-  // Debug mode is disabled by setting pin to LOW
+  // Debug mode is disabled by setting pin to LOW (use Jumper Cable)
   debug = digitalRead(DEBUG_PIN) != LOW;
   if(debug) {
     Serial.begin(9600);
   }
+
+  // Connect to DFPlayer via serial
   playerSerial.begin(9600);
-
-  print("Initializing...");
-  if (!dfPlayer.begin(playerSerial)) {
-    println("mp3 player serial error...");
-    // Fehler beim Initialisieren des DFPlayers
-    while (true) {
-      digitalWrite(LED_BUILTIN, HIGH);  // LED bleibt an, um Fehler anzuzeigen
-      delay(200);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(200);
-    }
+  println("Initializing...");
+  if (!dfPlayer.begin(playerSerial, false, true)) {
+    println("=> mp3 player serial error.");
+    println("=> Stopping...");
+    errorState();
   }
-
-  print("player connected...");
+  println("=> Player connected...");
   
+  // Player configuration
   dfPlayer.volume(PLAYER_VOLUME);
   dfPlayer.outputDevice(DFPLAYER_DEVICE_SD);
   dfPlayer.EQ(DFPLAYER_EQ_JAZZ);
@@ -75,6 +75,40 @@ void setup() {
   
   delay(INIT_DELAY);
   
+  // Wait until DFPlayer becomes available
+  println("=> Waiting for player become available...");
+  while(!dfPlayer.available()) {
+    delay(100);
+  }
+
+  // Read filecount from sd card
+  println("=> Reading filecount...");
+  dfPlayer.readState();
+  dfPlayer.read();
+  dfPlayer.readFileCounts(DFPLAYER_DEVICE_SD);
+  fileCount = dfPlayer.readFileCountsInFolder(0);
+  println("=> Found files: " + String(fileCount) + "...");
+
+  // Filecount is bigger than 0: enable random mode
+  // Filecount is -1: notify & continue with disabled random mode
+  // Filecount is 0: notify & errorState, cause no files found
+  if(fileCount > 0) {
+    useRandom = true;
+    println("=> Using random mode");
+  } else if(fileCount == -1) {
+    println("=> Reading filecount failed. No random mode available.");
+  } else {
+    println("=> No files on SD Card found.");
+    println("=> Stopping");
+    errorState();
+  }
+
+  // Initialize random with analog value as seed
+  int seed = analogRead(1);
+  randomSeed(seed);
+  println("=> Using random seed: " + String(seed));
+
+  // Initial playback delay
   lastTrackFinished = millis();
   println("Done.");
 }
@@ -103,9 +137,15 @@ void loop() {
     print("Motion detected...");
     bool isInTrackDelay = msSinceLastTrackFinished <= TRACK_DELAY;
     if(!isPlaying && !isInTrackDelay) {
-      print("Play track...");
       isPlaying = true;
-      dfPlayer.next();
+      if(useRandom) {
+        int trackNr = random(fileCount);
+        print("Play random track (" + String(trackNr) + ")...");
+        dfPlayer.play(trackNr);
+      } else {
+        print("Play track...");
+        dfPlayer.next();
+      }
       delay(PLAYER_PLAY_DELAY);
     } else if(isPlaying) {
       print("Still playing...");
@@ -118,6 +158,16 @@ void loop() {
 
   println("Loop done.");
   delay(LOOP_DELAY);
+}
+
+void errorState() {
+  // Start blinking built-in LED to visualize error state
+  while (true) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(200);
+  }
 }
 
 void print(String m) {
